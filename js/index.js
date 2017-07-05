53a14b957103f3e63f90623ebbbe5c64fea4b1e0
Alexeggers <alex.f.eggers@gmail.com>
2017-07-05 17:18:14 +0200

    init

---
+++ js/index.js
@@ -0,0 +1,23 @@
+/*global $, jQuery, alert*/
+$(document).ready(function() {
+    var locked = true,
+        currentStatusText = "Current Status: ",
+        lockedText = "Locked",
+        unlockedText = "Unlocked";
+    
+    function setLocked() {
+        if (locked) {
+            $('.door-status p').text(currentStatusText + "Locked");
+            $('.door-button').removeClass('unlocked');
+            $('.door-button').addClass('locked');
+        } else {
+            $('.door-status p').text(currentStatusText + "Unlocked");
+            $('.door-button').removeClass('locked');
+            $('.door-button').addClass('unlocked');
+        }
+        
+        locked = !locked;
+    }
+    setLocked();
+    $('.door-button').click(setLocked).bind(this, true);
+});
