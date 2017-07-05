b71d90682e1a362ccaf41f621ffd66329d03019b
alexeggers <alex.f.eggers@gmail.com>
2017-07-05 18:36:54 +0200

    Added colors for open and locked

--- js/index.js
+++ js/index.js
@@ -3,16 +3,16 @@ $(document).ready(function () {
     "use strict";
     var locked = true,
         currentStatusText = "Current Status: ",
-        lockedText = "Locked",
-        unlockedText = "Unlocked";
+        lockedText = "<span style='color: red'>Locked</span>",
+        unlockedText = "<span style='color: limegreen'>Open</span>";
     
     function setLocked() {
         if (locked) {
-            $('.door-status p').text(currentStatusText + "Locked");
+            $('.door-status p').html(currentStatusText + lockedText);
             $('.door-button').removeClass('unlocked');
             $('.door-button').addClass('locked');
         } else {
-            $('.door-status p').text(currentStatusText + "Unlocked");
+            $('.door-status p').html(currentStatusText + unlockedText);
             $('.door-button').removeClass('locked');
             $('.door-button').addClass('unlocked');
         }
